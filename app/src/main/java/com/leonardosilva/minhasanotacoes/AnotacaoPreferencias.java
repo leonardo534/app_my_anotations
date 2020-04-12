d680bc839aeda0b9e6a69b31cf9190bd0052f064
leonardo534 <leonardoredes534@gmail.com>
2020-04-12 15:51:31 -0300

    Classe criada para salvar as anotações

---
+++ app/src/main/java/com/leonardosilva/minhasanotacoes/AnotacaoPreferencias.java
@@ -0,0 +1,29 @@
+package com.leonardosilva.minhasanotacoes;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class AnotacaoPreferencias {
+    private static final String NOME_ARQUIVO = "anotacao.preferencia";
+    private Context context;
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
+    private final String CHAVE_NOME = "nome";
+
+
+    public AnotacaoPreferencias(Context c) {
+        this.context = c;
+        preferences = context.getSharedPreferences(NOME_ARQUIVO, 0);
+        editor = preferences.edit();
+    }
+
+    public void salvarAnotacao(String anotacao) {
+        editor.putString(CHAVE_NOME, anotacao);
+        editor.apply();
+    }
+
+    public String recuperarAnotacao() {
+
+        return "";
+    }
+}
