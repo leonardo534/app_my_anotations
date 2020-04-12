59bd335484cceb451abc4541b6f2c34ce209fa6b
leonardo534 <leonardoredes534@gmail.com>
2020-04-12 16:58:57 -0300

    Configurado metodos para verificar se o usuario digitou algo e se o arquivo exites, configurando tambem para mostrar o valor salvo

--- app/src/main/java/com/leonardosilva/minhasanotacoes/MainActivity.java
+++ app/src/main/java/com/leonardosilva/minhasanotacoes/MainActivity.java
@@ -11,20 +11,31 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private AnotacaoPreferencias preferencias;
+    private EditText editAnotacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editAnotacao = findViewById(R.id.editAnotacao);
+        preferencias = new AnotacaoPreferencias(this);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                if (editAnotacao.getText().toString().equals("")) {
+                    Snackbar.make(view, "Preencha a anotação", Snackbar.LENGTH_LONG).show();
+
+                } else {
+                    preferencias.salvarAnotacao(editAnotacao.getText().toString());
+                    Snackbar.make(view, "Salvo com sucesso", Snackbar.LENGTH_LONG).show();
+                }
+
             }
         });
     }
